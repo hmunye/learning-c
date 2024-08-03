1089d0a0603c4f8d89987ef43bc7461ba8f90ec0
Huseen <136820327+hmunye@users.noreply.github.com>
2024-08-02 21:04:10 -0400

    update to chapter 1, partial chapter 2

--- chapter_1/hello.c
+++ chapter_1/hello.c
@@ -11,16 +11,22 @@
 // 'void' within the parameters of 'main' indicates that the program does not
 // take any arguments. 'main' is defined to return an 'int' value (signed
 // integer type)
+//
+// This is the main entry point of the program, either in a freestanding or
+// hosted environment
 
 int main(void) {
   // The 'puts' function writes a string argument to the 'stdout' (standard
   // output)
   // Also appends a newline character '\n' at the end of the argument
-  // The return value from 'puts' is an int value. On failure, it returns
-  // the macro 'EOF' (A negative int value)
+  // The "Hello, World" argument is a string literal which behaves like a
+  // read-only string
+  // The return value from 'puts' is an int value. On failure,
+  // it returns the macro 'EOF' (A negative integer value), or a nonnegative
+  // integer
 
   if (puts("Hello, World") == EOF) {
-    // Evaluates to a nonzero value
+    // Expands to a nonzero value
     return EXIT_FAILURE;
   }
 
@@ -28,10 +34,17 @@ int main(void) {
   // function to read the second argument as a string literal and print it to
   // stdout with a new line after the string
   //
-  // 'printf' will return the number of charaters printed if successful or a
-  // negative value if an error occured
-  (void) printf("%s\n", "Hello World");
+  // "Take care not to pass user-supplied data as part of the first argument to
+  // the printf function, because doing so can result in a formatted output
+  // security vulnerability (Seacord 2013)"
+  //
+  // 'printf' will return the number of characters printed if successful or a
+  // negative value if an error occurred
+  if (printf("%s\n", "Hello World") < 0) {
+    return EXIT_FAILURE;
+  }
 
-  // 'EXIT_SUCCESS' commonly expands to the value 0
+  // 'EXIT_SUCCESS' is an object-like macro that commonly expands to the value 0
+  // Each occurrence of this macro is replaced by a 0
   return EXIT_SUCCESS;
 }
