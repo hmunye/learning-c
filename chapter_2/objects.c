f70443d27dbc283a7ecb4f2097ebbc0502872454
Huseen <136820327+hmunye@users.noreply.github.com>
2024-08-05 17:27:36 -0400

    chapter 2 updated

--- chapter_2/objects.c
+++ chapter_2/objects.c
@@ -1,8 +1,15 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 // Function declarations (not definitions)
-int addition(int, int);
+void addition(int, int);
+void boolean(void);
+void chars(void);
+void nums(void);
 void swap(int *, int *);
+void swap_v2(int *, int *);
+
+enum days { sun, mon, tue, wed, thu, fri, sat };
 
 int main(void) {
   // Objects are storage in which you can represent values
@@ -13,20 +20,47 @@ int main(void) {
   int x = 5;
   int y = 10;
 
-  printf("%d\n", addition(x, y));
-
   printf("Before swap: x = %d, y = %d\n", x, y);
-  swap(&x, &y);
+  swap_v2(&x, &y);
   printf("After swap: x = %d, y = %d\n", x, y);
+
+  for (int i = 0; i < 5; i++) {
+    addition(x, y);
+  }
 }
 
 // Functions are not objects but do have types
 // They are characterized by both the return type as well as the number and
 // types of it's arguments
-int addition(int x, int y) {
+void addition(int x, int y) {
+  // Static objects must be initialized with a constant value
   static unsigned int value = 4;
 
-  return value + x + y;
+  value += 1;
+
+  printf("%d\n", value + x + y);
+}
+
+void boolean(void) {
+  _Bool flag1 = 0;    // Can be 1 or 0
+  bool flag2 = false; // Can be true or false
+}
+
+void chars(void) {
+  signed char letter = '1';
+  unsigned char letter2 = '3';
+  char letter3 = 'a';
+}
+
+void nums(void) {
+  signed int num = -2;
+  unsigned int num2 = 4;
+  int num3 = 5;
+
+  short int num4 = 7;
+  long int num5 = 0;
+  long long int num6 = 94;
+  long num7 = 30;
 }
 
 void swap(int *px, int *py) {
@@ -35,6 +69,15 @@ void swap(int *px, int *py) {
   int temp = *px;
   *px = *py;
   *py = temp;
+
+  return;
+}
+
+// Could probably be better
+void swap_v2(int *px, int *py) {
+  *px = *px + *py;
+  *py = *px - *py;
+  *px = *px - *py;
 
   return;
 }
